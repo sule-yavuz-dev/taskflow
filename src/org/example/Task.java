02282e3f0a4a2e1216b7629168dbf8ae33e9d35e
sule <sule.yavuz.dev@gmail.com>
2026-03-24 15:23:26 +0300

    feat: create Task entity with fields, constructor and basic behavior

---
+++ src/org/example/Task.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private int priority;
+    private boolean completed;
+
+
+    public Task(int id, String title, String description, int priority) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.completed = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markAsCompleted(){
+        this.completed = true;
+    }
+}
