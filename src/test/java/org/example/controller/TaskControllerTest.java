c5dd84e53d5a511278a12066289a1a8812ba890f
sule <sule.yavuz.dev@gmail.com>
2026-05-13 13:34:27 +0300

    test: add MockMvc tests for task controller

---
+++ src/test/java/org/example/controller/TaskControllerTest.java
@@ -0,0 +1,75 @@
+package org.example.controller;
+
+import org.example.exception.TaskNotFoundException;
+import org.example.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(TaskController.class)
+class TaskControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TaskService taskService;
+
+    @Test
+    void getTasksReturnsOk() throws Exception {
+        mockMvc.perform(get("/tasks")).andExpect(status().isOk());
+    }
+
+    @Test
+    void getMissingTask() throws Exception {
+        when(taskService.getTaskById(999L)).thenThrow(new TaskNotFoundException("Task not found with id: 999"));
+        mockMvc.perform(get("/tasks/999")).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void updateTaskReturnOk() throws Exception {
+        String requestBody = """
+                {
+                "title": "Updated Task",
+                "description": "Updated description",
+                "priority":2
+                }
+                """;
+        mockMvc.perform(put("/tasks/1").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isOk());
+
+    }
+
+    @Test
+    void createTaskReturnsCreated() throws Exception {
+        String requestBody = """
+                {
+                "title": "New Task",
+                "description": "Created from test",
+                "priority":1
+                }
+                """;
+        mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isCreated());
+    }
+
+    @Test
+    void createdTaskReturnsBadRequest() throws Exception {
+        String requestBody = """
+                {
+                "title": "",
+                "description": "Invalid task",
+                "priority": 1
+                }
+                """;
+        mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(requestBody)).andExpect(status().isBadRequest());
+    }
+}
